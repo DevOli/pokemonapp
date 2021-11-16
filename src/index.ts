dad79b68d70e6dc963c13648177b04597db3ffe3
Oliver Rivas <oliver.rivas@jala-foundation.org>
2021-11-16 17:55:56 -0400

    Add API service functionality. Add routes

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,28 @@
-import PokemonTypeController from './controllers/PokemonTypeController';
-import PokemonController from './controllers/PokemonController';
-import PokemonTypeApi from './api/PokemonApi';
+import path from "path"
+const express = require('express')
+const app = express()
+const port = 8080
 
-//PokemonTypeController.showAllPokemonTypes();
-//PokemonController.showAllPokemon();
+var pokemonRoute = require('./routes/pokemon')
+var typeRoute = require('./routes/type')
 
-//Get console arguments
-var type = process.argv.slice(2);
-console.log('You selected type: ', type[0]);
+app.get('/', function (req: any, res: any) {
+  res.sendFile(path.join(__dirname, '/views/index.html'))
+})
 
-PokemonTypeController.showPokemonType(type[0])
-    .then(pokemonName =>{
-        PokemonController.showPokemon(pokemonName);
-    })
-    .catch(err => console.log(err))
+app.use('/api/v2/pokemon', pokemonRoute);
+app.use('/api/v2/type', typeRoute);
+
+app.listen(port, () => {
+  console.log(`Pokedex App listening at http://localhost:${port}`);
+});
+
+//Console APP
+// var type = process.argv.slice(2);
+// console.log('You selected type: ', type[0]);
+
+// PokemonTypeController.showPokemonType(type[0])
+//     .then(pokemonName =>{
+//         PokemonController.showPokemon(pokemonName);
+//     })
+//     .catch(err => console.log(err))
---
+++ src/routes/pokemon.ts
@@ -0,0 +1,33 @@
+
+import PokemonController from '../controllers/PokemonController';
+
+const express = require('express')
+const router = express.Router()
+
+// middleware that is specific to this router
+router.use(function timeLog (req: any, res: any, next: any) {
+  console.log('Time: ', Date.now())
+  next()
+})
+// all pokemons
+router.get('/', async function (req: any, res: any) { 
+  try {
+    const pokemons = await PokemonController.showAllPokemon();
+    res.send({count: pokemons.length, results: pokemons})
+  } catch (error) {
+    res.status(404).send("Oops something went wrong!")
+  }
+})
+// specific pokemon
+router.get('/:pokemon', async function (req: any, res: any) {
+  try {
+    const pokemon: string = req.params.pokemon
+    const pokemonResult = await PokemonController.showPokemon(pokemon)
+    res.send(pokemonResult)    
+  } catch (error) {
+    res.status(404).send("Oops something went wrong!")
+  }
+  
+})
+
+module.exports = router
