1d8ebfad29f24235a2287e7bdf655e1427e50373
Oliver Rivas <oliver.rivas@jala-foundation.org>
2021-11-16 11:15:31 -0400

    Changes to consume generic type Api, and return objects

--- src/controllers/PokemonController.ts
+++ src/controllers/PokemonController.ts
@@ -1,21 +1,48 @@
-import PokemonTypeApi from '../api/PokemonApi';
+import PokemonApi from '../api/PokemonApi';
+import Pokemon from '../interfaces/Pokemon';
+import PokemonType from '../interfaces/PokemonType';
+import PokemonModel from '../models/PokemonModel';
 
 export default class PokemonController {
 
-  static async showAllPokemon () : Promise<void> {
+  static async showAllPokemon () : Promise<PokemonModel[]> {
     try {
-      const pokemonTypes = await PokemonTypeApi.getAllPokemons();
-      pokemonTypes.forEach(pt => {
-        console.log(`${pt}`);
+      const pokemonTypes = await PokemonApi.getAll<PokemonModel, Pokemon>("pokemon", PokemonModel);
+      pokemonTypes.forEach(p => {
+        console.log(`${p}`);
       });
+      return pokemonTypes
     } catch (ex: any) {
       console.log(ex.message);
+      throw new Error('Something wrong happened in showAllPokemon');
     }
   }
 
-  static showPokemon(name: string){      
-    PokemonTypeApi.getPokemon(name)
-      .then((pokemonType) => console.log(`${pokemonType}`))
-      .catch(error => console.error(error));
+  static async showPokemon(name: string) : Promise<PokemonModel> {
+    try {
+      const pokemon = await PokemonApi.getOne<PokemonModel, Pokemon>("pokemon", name, PokemonModel)
+      console.log(`${pokemon}`)
+      return pokemon     
+    } catch (ex: any) {
+      console.error(ex);
+      throw new Error('Something wrong happened in showPokemon');
+    }         
   }
+    
+  // static async showAllPokemon () : Promise<void> {
+  //   try {
+  //     const pokemonTypes = await PokemonApi.getAllPokemons();
+  //     pokemonTypes.forEach(p => {
+  //       console.log(`${p}`);
+  //     });
+  //   } catch (ex: any) {
+  //     console.log(ex.message);
+  //   }
+  // }
+
+  // static showPokemon(name: string){      
+  //   PokemonApi.getPokemon(name)
+  //     .then((pokemonType) => console.log(`${pokemonType}`))
+  //     .catch(error => console.error(error));
+  // }
 }
--- src/controllers/PokemonTypeController.ts
+++ src/controllers/PokemonTypeController.ts
@@ -1,21 +1,27 @@
-import PokemonTypeApi from '../api/PokemonApi';
+import PokemonApi from '../api/PokemonApi';
+import Pokemon from '../interfaces/Pokemon';
+import PokemonType from '../interfaces/PokemonType';
+import PokemonModel from '../models/PokemonModel';
+import PokemonTypeModel from '../models/PokemonTypeModel';
 
 export default class PokemonTypeController {
 
-  static async showAllPokemonTypes () : Promise<void> {
+  static async showAllPokemonTypes () : Promise<PokemonTypeModel[]> {
     try {
-      const pokemonTypes = await PokemonTypeApi.getAllPokemonTypes();
+      const pokemonTypes = await PokemonApi.getAll<PokemonTypeModel, PokemonType>("type", PokemonTypeModel);
       pokemonTypes.forEach(pt => {
         console.log(`${pt}`);
       });
+      return pokemonTypes
     } catch (ex: any) {
       console.log(ex.message);
+      throw new Error('Something wrong happened in showAllPokemonTypes');
     }
   }
 
   static async showPokemonType(type: string) : Promise<string> {
     try {
-      const pokemonType = await PokemonTypeApi.getPokemonType(type);
+      const pokemonType = await PokemonApi.getOne<PokemonTypeModel, PokemonType>("type", type, PokemonTypeModel);
       console.log(`${pokemonType}`);
       return pokemonType.getRandomPokemonName();
     } catch (ex: any) {    
@@ -23,5 +29,27 @@ export default class PokemonTypeController {
       throw new Error('Something wrong happened in getPokemonType');
     }
   }
-  
+
+  // static async showAllPokemonTypes () : Promise<void> {
+  //   try {
+  //     const pokemonTypes = await PokemonTypeApi.getAllPokemonTypes();
+  //     pokemonTypes.forEach(pt => {
+  //       console.log(`${pt}`);
+  //     });
+  //   } catch (ex: any) {
+  //     console.log(ex.message);
+  //   }
+  // }
+
+  // static async showPokemonType(type: string) : Promise<string> {
+  //   try {
+  //     const pokemonType = await PokemonApi.getPokemonType(type);
+  //     console.log(`${pokemonType}`);
+  //     return pokemonType.getRandomPokemonName();
+  //   } catch (ex: any) {    
+  //     console.log(ex.message);
+  //     throw new Error('Something wrong happened in getPokemonType');
+  //   }
+  // }
+
 }
