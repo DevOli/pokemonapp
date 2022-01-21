421dc475920b6dbc707e2a16ef0d9309bb277bda
Oliver Rivas <oliver.rivas@jala-foundation.org>
2022-01-21 18:34:17 -0400

    Last commit

--- src/index.ts
+++ src/index.ts
@@ -36,13 +36,3 @@ app.use('/', indexRoute);
 app.listen(port, () => {
   console.log(`Pokedex App listening at http://localhost:${port}`);
 });
-
-//Console APP
-// var type = process.argv.slice(2);
-// console.log('You selected type: ', type[0]);
-
-// PokemonTypeController.showPokemonType(type[0])
-//     .then(pokemonName =>{
-//         PokemonController.showPokemon(pokemonName);
-//     })
-//     .catch(err => console.log(err))
